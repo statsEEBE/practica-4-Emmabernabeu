b679413ed95d8053fa5ff1ca593f3bd543f7d686
emma.bernabeu <emma.bernabeu@auleseebe.upc.edu>
2024-10-31 13:20:28 +0100

    ...

--- Pregunta2.R
+++ Pregunta2.R
@@ -1,1 +1,17 @@
+ # a) APARTADO 1 --> P(T>=13000) = 1- P(T<=13000)
+#Tiempo --> exp
+#E(T)=10000 =1/parametro
 
+1- pexp(13000,1/10000)
+
+# APARTADO 2 --> P(T<=t)=0.8
+qexp(0.2,1/10000)
+
+
+# b)APARTADO 1
+set.seed(85)
+simul <- rexp(100,1/10000)
+simul
+mean(simul)
+median(simul)
+var(simul)
